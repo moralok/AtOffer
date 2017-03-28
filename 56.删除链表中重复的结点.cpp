091f2d0b98fc5f3d38ee49c21fd3ba01fa024fe7
moralok <moralok@163.com>
2017-03-29 01:16:34 +0800

    Update 56.删除链表中重复的结点.cpp

--- 56.删除链表中重复的结点.cpp
+++ 56.删除链表中重复的结点.cpp
@@ -4,7 +4,7 @@
 例如，链表1->2->3->3->4->4->5 处理后为 1->2->5
 */
 
-//就算再自己写一遍也很难写出这么棒的递归啊，怎么这么弱555
+//算法1:就算再自己写一遍也很难写出这么棒的递归啊，怎么这么弱555
 /*
 struct ListNode {
     int val;
@@ -33,3 +33,29 @@ public:
         }
     }
 };
+//算法2:也很巧妙呢
+class Solution {
+public:
+    ListNode* deleteDuplication(ListNode* pHead)
+    {
+        ListNode* first=new ListNode(-1); //创建一个新结点接在最前面简化第一个结点要去除的情况
+        first->next=pHead;
+        ListNode *p=pHead;		  //指针可在创建时赋值，作为前头兵
+        ListNode *last=first;	          //作为跟踪连接的指针
+        while(p && p->next){
+            if(p->val==p->next->val){
+                int val=p->val;
+                while(p && p->val==val){
+                    p=p->next;
+                }
+                last->next=p;
+            }else{
+                last=p;				//要连接新结点才用next
+                p=p->next;
+            }
+        }
+        return first->next;
+    }
+};
+
+
